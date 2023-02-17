80f5412154e69e472e7ec5f6f0544d63172e5f55
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-16 22:51:26 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a long long: %lu byte(s)\n", (unsigned long)sizeof(c));
-printf("size of a float: 4 byte(s)\n", (unsigned long)sizeof(f));
+printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
 
