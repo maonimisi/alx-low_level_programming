87e690a8ae8c489661bf42363182093e65903a41
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-18 04:38:43 -0800

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d", digit);
+	printf("\n");
+	return (0);
+}
