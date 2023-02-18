52134a5810ff61bfbcbadaa9833b3478f48597ce
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-18 09:10:03 -0800

    print numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,14 +2,14 @@
 /**
  * main - Prints all single digit numbers of base 10 starting from 0.
  *
- * return - Always return 0
+ * Return: Always return 0
  */
 int main(void)
 {
 	int num;
 
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
-	putchar('\n');
+	putchar((num % 10) + '0');
+	putchar("\n");
 	return (0);
 }
