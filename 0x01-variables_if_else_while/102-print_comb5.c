2b4ef9d72f5bf1de0af0e4fac0acd173f6c3429e
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-20 08:10:41 -0800

    combination of two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+*main - this program prints all combination of two digits from 0 - 99
+*
+*Return: alway 0 (success)
+*/
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 < 100; num1++)
+	{
+		for (num2 = 0; num2 < 100; num2++)
+		{
+			if (num1 < num2)
+			{
+			putchar((num1 / 10) + 48);
+			putchar((num1 % 10) + 48);
+			putchar(' ');
+			putchar((num2 / 10) + 48);
+			putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);	
+}
