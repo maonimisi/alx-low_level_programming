5630c8127fb628c7b951e5684b336423ea8a9e9c
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-27 04:22:29 -0800

    Reset number

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* reset_to_98 - takes the pointer and update the value to n
+* @n: the integer to be checked
+* Return: Always 0.
+*/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory consist of task from pointers, arrays and strings
