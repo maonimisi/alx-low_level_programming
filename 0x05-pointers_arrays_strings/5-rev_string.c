5132d6872a8eca238944d85b2fd4c3d6ba9926fb
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-27 08:37:59 -0800

    forward and reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* rev_string - this function reverse string
+* @s: the string to be reverse
+* Return: Always 0.
+*/
+void rev_string(char *s)
+{
+	int count = 0, i, temp = 0;
+
+	while (*(s + count) != '\0')
+	{
+		count++;
+	}
+	for (i = 0 ; i <= ((count - 1) / 2) ; i++)
+	{
+		temp = *(s + i);
+		*(s + i) = *(s + (count - (i + 1)));
+		*(s + (count - (i + 1))) = temp;
+	}
+}
+
