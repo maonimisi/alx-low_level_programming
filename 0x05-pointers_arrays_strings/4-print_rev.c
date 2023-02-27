7257344796cecd618e1b29ab8aa9406e4491e0cd
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-27 06:39:30 -0800

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* print_rev - this function prints string in reverse 
+* @s: the string to be printed
+* Return: Always 0.
+*/
+void print_rev(char *s)
+{
+	int i, j;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+		j = i - 1;
+		while (j >= 0 )
+		{
+			_putchar(s[j]);
+			j--;
+		}
+	_putchar('\n');
+}
