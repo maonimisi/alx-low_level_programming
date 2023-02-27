e5d7c6eac632e0bfd23b686b6a29aab0373da308
maonimisi <mabdulrasheed1210@gmail.com>
2023-02-27 09:39:03 -0800

    skipped character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,8 @@ void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
-		i++;
-	}
+	for (i = 0; str[i] != '\0'; i++)
+	if (i % 2 == 0)
+		_putchar(str[i]);
 	_putchar('\n');
 }
