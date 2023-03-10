7a3e373fbd12774216c968ba356dcd2ff4ec9f10
maonimisi <mabdulrasheed1210@gmail.com>
2023-03-10 06:18:16 -0800

    print recieved argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - this function prints all arguments passed to it
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+* Return: Always 0
+*/
+int main (int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
