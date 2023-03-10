230006e7fba854f52135f7c88930b89b0037493e
maonimisi <mabdulrasheed1210@gmail.com>
2023-03-10 05:51:37 -0800

    what is my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,10 @@
 * @argv: argument vector
 * Return: Always 0.
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	(void)argc;
+
 	printf("%s\n", argv[0]);
 	return (0);
 }
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This directory contains project on argc and argv
