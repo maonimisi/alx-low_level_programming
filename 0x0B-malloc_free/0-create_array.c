acfdf11118be9f421a6cae6a4ecc1530d626d5f8
maonimisi <mabdulrasheed1210@gmail.com>
2023-03-13 02:38:58 -0700

    create array of chr

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* create_array - this function creates an array of characters
+* @size: size of memory
+* @c: character to prints
+* return: Return char
+*/
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *j;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	j = malloc(sizeof(char) * size);
+
+	if (j == 0)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		j[i] = c;
+	return (j);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+This directory contains projects from memory allocation
