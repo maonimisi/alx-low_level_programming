a2dbcf44345daae01e6cef4ceb81788b606777f9
maonimisi <mabdulrasheed1210@gmail.com>
2023-05-08 05:32:48 -0700

    file i/o

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,63 +1,102 @@
 #include "main.h"
-#define BUFFER_SIZE 1024
 /**
- * main - copies the content from one file to another
- * @argc: number of arguments passed
- * @argv: argument vector
- * Return: Return 0 on success, exit codes on error
+ * checkArgumentCount - checks for the correct number of arguments
+ * @argc: number of arguments
+ * Return: Void.
  */
-int main(int argc, char **argv)
+void checkArgumentCount(int argc)
 {
-	int fd_from, fd_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+}
+
+/**
+ * checkFileFrom - checks that file from exist and can be read
+ * @result: if success or failed.
+ * @filename: name of file
+ * Return: Void.
+ */
+void checkFileFrom(int result, const char *filename)
+{
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+}
+/**
+ * checkFileTo - checks that file to exist and can be writting to
+ * @result: if success or failed
+ * @filename: name of file
+ * @fileFromDescriptor: file descriptor from
+ * Return: Void.
+ */
+void checkFileTo(int result, const char *filename, int fileFromDescriptor)
+{
+	if (result == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		close(fd_from);
-		return (99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+		close(fileFromDescriptor);
+		exit(99);
 	}
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+}
+
+/**
+ * checkClose - checks that file to exist and can be writting
+ * @result: if success or failed
+ * @fileDescriptor: file descriptor to
+ * Return: Void.
+ */
+void checkClose(int result, int fileDescriptor)
+{
+	if (result == -1)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written == -1 || bytes_written != bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			return (99);
-		}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileDescriptor);
+		exit(100);
 	}
-	if (bytes_read == -1)
+}
+
+/**
+ * main - opies the content of a file to another file.
+ * @argc: number of arguments passed
+ * @argv: array of pointers to the arguments
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int fileFromDescriptor, fileToDescriptor, closeResult;
+	ssize_t bytesRead, bytesWritten;
+	char buffer[1024];
+	mode_t filePermissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	checkArgumentCount(argc);
+
+	fileFromDescriptor = open(argv[1], O_RDONLY);
+	checkFileFrom(fileFromDescriptor, argv[1]);
+
+	fileToDescriptor = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+				filePermissions);
+	checkFileTo(fileToDescriptor, argv[2], fileFromDescriptor);
+
+	bytesRead = 1024;
+	while (bytesRead == 1024)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		return (98);
+		bytesRead = read(fileFromDescriptor, buffer, 1024);
+		checkFileFrom(bytesRead, argv[1]);
+
+		bytesWritten = write(fileToDescriptor, buffer, bytesRead);
+		if (bytesWritten != bytesRead)
+			bytesWritten = -1;
+		checkFileTo(bytesWritten, argv[2], fileFromDescriptor);
 	}
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_from);
-		close(fd_to);
-		return (100);
-	}
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd_to);
-		return (100);
-	}
+	closeResult = close(fileToDescriptor);
+	checkClose(closeResult, fileToDescriptor);
+
+	closeResult = close(fileFromDescriptor);
+	checkClose(closeResult, fileFromDescriptor);
+
 	return (0);
 }
