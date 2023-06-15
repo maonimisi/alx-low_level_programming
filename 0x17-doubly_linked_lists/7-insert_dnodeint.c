24f5b1820f3b0f4960f597147c61e769610d48d7
maonimisi <mabdulrasheed1210@gmail.com>
2023-06-15 06:22:27 -0700

    insert dnodeint

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,7 +28,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 			new_node->prev = current_node;
 			if (current_node->next != NULL)
 				current_node->next->prev = new_node;
-				current_node->next = new_node;
+			current_node->next = new_node;
 			return (new_node);
 		}
 		current_node = current_node->next;
